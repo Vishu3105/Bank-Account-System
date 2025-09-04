030804db38116c827485b6c2e7d0a41bca566ffe
Vishu3105 <vishwass2003@gmail.com>
2025-09-04 22:46:32 +0530

    created class

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,25 @@
+class BankAccount {
+    private String accountHolder;
+    private int accountNumber;
+    private double balance;
+
+    // Constructor
+    public BankAccount(String accountHolder, int accountNumber, double initialBalance) {
+        this.accountHolder = accountHolder;
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+    }
+
+    // Deposit
+    public void deposit(double amount) {
+        if (amount <= 0) {
+            System.out.println("Deposit amount must be positive.");
+            return;
+        }
+        balance += amount;
+        System.out.println(amount + " deposited successfully into " + accountHolder + "'s account.");
+    }
+}
 public class Main {
     public static void main(String[] args) {
 
